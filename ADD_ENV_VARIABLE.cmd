bc7019ebcc413c7d9b1b25799617c888ac3e7350
Hannah Twigg-Smith <htwiggsmith@gmail.com>
2022-07-28 00:39:26 -0700

    Made a drawy thing!

--- ADD_ENV_VARIABLE.cmd
+++ ADD_ENV_VARIABLE.cmd
@@ -3,7 +3,7 @@
 :: Replace YOUR CUSTOM SDK PATH HERE with your custom path.
 :: Use \ as separator
 :: Must not end with \
-set SDKPATH="YOUR CUSTOM SDK PATH HERE"
+set SDKPATH=C:\Program Files\PlaydateSDK
 
 
 if %SDKPATH% == "YOUR CUSTOM SDK PATH HERE" (
--- source/main.lua
+++ source/main.lua
@@ -1,28 +1,68 @@
-import "dvd" -- DEMO
-local dvd = dvd(1, -1) -- DEMO
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/timer"
+import "CoreLibs/crank"
 
 local gfx <const> = playdate.graphics
-local font = gfx.font.new('font/Mini Sans 2X') -- DEMO
 
-local function loadGame()
-	playdate.display.setRefreshRate(50) -- Sets framerate to 50 fps
-	math.randomseed(playdate.getSecondsSinceEpoch()) -- seed for math.random
-	gfx.setFont(font) -- DEMO
+local objectSize = { x = 1, y = 1 }
+local origin = { x = 200, y = 120 }
+local changeModifier = 2
+
+function drawCenteredRect()
+    gfx.drawRect(origin.x - objectSize.x / 2, origin.y - objectSize.y / 2, objectSize.x, objectSize.y)
 end
 
-local function updateGame()
-	dvd:update() -- DEMO
+function updateObjectSize(change)
+    objectSize.x += change / changeModifier
+    objectSize.y += change / changeModifier
 end
 
-local function drawGame()
-	gfx.clear() -- Clears the screen
-	dvd:draw() -- DEMO
+function moveObject(dx, dy)
+    origin.x += dx
+    origin.y += dy
 end
 
-loadGame()
+function clear()
+    gfx.clear()
+end
 
+--- The setup function for the game environment.
+function setup()
+    gfx.setColor(gfx.kColorBlack)
+    drawCenteredRect()
+end
+
+setup()
+
+--- Callback that runs when the crank is cranked.
+function playdate.cranked(change, acceleratedChange)
+    updateObjectSize(acceleratedChange)
+    drawCenteredRect()
+end
+
+--- Game loop function.
 function playdate.update()
-	updateGame()
-	drawGame()
-	playdate.drawFPS(0,0) -- FPS widget
+
+    if playdate.buttonIsPressed(playdate.kButtonUp) then
+        moveObject(0, -2)
+    end
+
+    if playdate.buttonIsPressed(playdate.kButtonRight) then
+        moveObject(2, 0)
+    end
+
+    if playdate.buttonIsPressed(playdate.kButtonDown) then
+        moveObject(0, 2)
+    end
+
+    if playdate.buttonIsPressed(playdate.kButtonLeft) then
+        moveObject(-2, 0)
+    end
+
+    if playdate.buttonIsPressed(playdate.kButtonB) then
+        clear()
+    end
+
+    playdate.timer.updateTimers()
 end
